00cd84cac9f208348991c6b4bc0c73d6b46b0eb1
[SW5] 박유나 <jeannelvn@gmail.com>
2023-08-14 09:56:19 +0900

    add: form event에 TypeScript 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,43 @@
+import { useState } from "react";
+
 function App() {
-    return <div></div>;
+    const [inpval, setInpval] = useState({
+        id: "",
+        password: "",
+    });
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.currentTarget;
+        setInpval((prev) => ({ ...prev, [name]: value }));
+        // if (name === "id") setInpval(value);
+        // if (name === "password") setInpval(value);
+    };
+
+    const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log("logged in");
+    };
+
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input
+                    type="text"
+                    name="id"
+                    value={inpval.id}
+                    onChange={onChange}
+                    placeholder="id"
+                />
+                <input
+                    type="password"
+                    name="password"
+                    value={inpval.password}
+                    onChange={onChange}
+                    placeholder="password"
+                />
+                <button type="submit">로그인</button>
+            </form>
+        </div>
+    );
 }
 
 export default App;
