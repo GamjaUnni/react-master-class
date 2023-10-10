2bf677ee890891d3b4666c0c0849c8e2d665ca67
[SW5] 박유나 <jeannelvn@gmail.com>
2023-10-10 15:49:53 +0900

    add: animation 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,41 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 const Father = styled.div`
     display: flex;
 `;
 
+const rotationAnimation = keyframes`
+	0%{
+		transform: rotate(0deg);
+		border-radius: 0px;
+	}
+	50%{
+		border-radius: 100px;
+	}
+	100%{
+		transform: rotate(360deg);
+		border-radius: 0px;
+	}
+`;
+
 const Box = styled.div<{ bgColor: string }>`
+    display: flex;
+    align-items: center;
+    justify-content: center;
     background-color: ${(props) => props.bgColor}; // props 전달 받기
-    width: 100px;
-    height: 100px;
+    width: 200px;
+    height: 200px;
+    animation: ${rotationAnimation} 1s linear infinite;
+
+    span {
+        font-size: 36px;
+        &:hover {
+            font-size: 48px;
+        }
+        &:active {
+            opacity: 0;
+        }
+    }
 `;
 
 // 컴포넌트 확장(상속) : Box와 동일한 속성 + border-radius 속성 추가
@@ -15,12 +43,12 @@ const Circle = styled(Box)`
     border-radius: 50%;
 `;
 
-// const Btn = styled.button`
-//     color: #fff;
-//     background-color: tomato;
-//     border: 0;
-//     border-radius: 15px;
-// `;
+const Btn = styled.button`
+    color: #fff;
+    background-color: tomato;
+    border: 0;
+    border-radius: 15px;
+`;
 
 const Input = styled.input.attrs({ required: true, maxLength: 10 })`
     background-color: orange;
@@ -28,18 +56,22 @@ const Input = styled.input.attrs({ required: true, maxLength: 10 })`
 
 function App() {
     return (
-        <Father as="header">
-            <Box bgColor="teal" />
-            <Circle bgColor="whitesmoke" />
-            {/* <Btn>Login</Btn>
+        <>
+            <Father as="header">
+                <Box bgColor="teal">
+                    <span>😎</span>
+                </Box>
+                <Circle bgColor="whitesmoke" />
+            </Father>
+            <Btn>Login</Btn>
 
             <Btn as="a" href="/">
                 Login
-            </Btn> */}
+            </Btn>
 
             <Input />
             <Input />
-        </Father>
+        </>
     );
 }
 
