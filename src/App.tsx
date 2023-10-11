463ccf3b739e55984da1aed45f7d1ade221f59e3
[SW5] 박유나 <jeannelvn@gmail.com>
2023-10-11 11:12:19 +0900

    feat: state에 type 지정하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
 import Circle from "./Circle";
 
 function App() {
+    const [counter, setCounter] = useState<number | string>(1);
+    setCounter(2);
+    setCounter("hello world!");
+
     return (
         <div>
             <Circle bgColor="teal" borderColor="#000" />
