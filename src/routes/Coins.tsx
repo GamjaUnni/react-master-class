474c91794523f525904e1f85e42ac043292656a0
[SW5] 박유나 <jeannelvn@gmail.com>
2023-10-20 13:40:04 +0900

    feat: fetcher 함수 정의

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
+import { useQuery } from "react-query";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { fetchCoins } from "../api";
 
 const Container = styled.div`
     padding: 0 20px;
@@ -51,7 +53,7 @@ const Img = styled.img`
     margin-right: 10px;
 `;
 
-interface CoinInterface {
+interface ICoin {
     id: string;
     name: string;
     symbol: string;
@@ -62,20 +64,22 @@ interface CoinInterface {
 }
 
 function Coins() {
-    const [coins, setCoins] = useState<CoinInterface[]>([]);
+    // const [coins, setCoins] = useState<CoinInterface[]>([]);
 
-    const [loading, setLoading] = useState(true);
+    // const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        (async () => {
-            const response = await fetch(
-                "https://api.coinpaprika.com/v1/coins"
-            );
-            const json = await response.json();
-            setCoins(json.slice(0, 100));
-            setLoading(false);
-        })();
-    }, []);
+    // useEffect(() => {
+    //     (async () => {
+    //         const response = await fetch(
+    //             "https://api.coinpaprika.com/v1/coins"
+    //         );
+    //         const json = await response.json();
+    //         setCoins(json.slice(0, 100));
+    //         setLoading(false);
+    //     })();
+    // }, []);
+
+    const { isLoading, data } = useQuery<ICoin[]>(["allCoins"], fetchCoins);
 
     return (
         <Container>
