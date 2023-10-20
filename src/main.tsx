ff01a90c2b1f0981bcc9a82b13314143a76cb3b6
[SW5] 박유나 <jeannelvn@gmail.com>
2023-10-20 13:28:02 +0900

    react-query 설치

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { ThemeProvider } from "styled-components";
+import App from "./App";
 import { theme } from "./theme";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
-        <ThemeProvider theme={theme}>
-            <App />
-        </ThemeProvider>
+        <QueryClientProvider client={queryClient}>
+            <ThemeProvider theme={theme}>
+                <App />
+            </ThemeProvider>
+        </QueryClientProvider>
     </React.StrictMode>
 );
